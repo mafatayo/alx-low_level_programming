87886f7630a6a39f96bc6ae92226e6f38e2a9fc7
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-13 11:34:03 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
 /**
  * main - Print all var types
  *
- * Return: Always 0 (Succes)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -14,10 +14,10 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %dbyte(s)\n",sizeof(a));
-	printf("Size of an int: %dbyte(s)\n",sizeof(b));
-	printf("Size of a long int: %dbyte(s)\n",sizeof(c));
-	printf("Size of a long long int: %dbytes(s)\n",sizeof(d));
-	printf("Size of a float: %dbytes(s)\n",sizeof(e));
+	printf("Size of a char: %dbyte(s)\n", sizeof(a));
+	printf("Size of an int: %dbyte(s)\n", sizeof(b));
+	printf("Size of a long int: %dbyte(s)\n", sizeof(c));
+	printf("Size of a long long int: %dbytes(s)\n", sizeof(d));
+	printf("Size of a float: %dbytes(s)\n", sizeof(e));
 	return (0);
 }
