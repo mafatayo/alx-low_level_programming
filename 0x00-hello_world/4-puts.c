8eb8bd8e3ab20f4f658f67d3660c3a83277accc7
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-13 08:34:53 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/** 
+ * main - Printing in C
+ *
+ * Return: Always 0 (Succesful)
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return(0);
+}
