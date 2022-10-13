a0cff18ad79df35e31b9dc6d916dca2a30359372
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-13 09:10:43 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints out text
+ *
+ * Return: Always 0 (Approved)
+ */
+int main(void)
+{
+     printf("with proper grammar, but the outcome is a piece of art,\n");
+     return(0);
+}
