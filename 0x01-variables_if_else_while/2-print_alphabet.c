80cc69b84d0cfb71dd0a8a93d32f263efcc7b9aa
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-14 13:23:49 -0700

    Task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ak;
+
+	for(ak = 'a'; ak <= 'z'; ak++)
+	{
+		putchar(ak);
+	}
+	putchar('\n');
+	return (0);
+}
