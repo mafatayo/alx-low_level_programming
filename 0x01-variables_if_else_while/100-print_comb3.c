4b7b6472b059f2109f46de5921f5b43ae27bea0a
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-15 10:20:26 -0700

    Task 10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,8 +12,8 @@ int main(void)
 	{
 		for (num2 = num1 + 1; num2 <= 10; num2++)
 		{
-			putchar(num1 % 10);
-			putchar(num2 % 10);
+			putchar((num1 % 10) + '0');
+			putchar((num2 % 10) + '0');
 
 			if (num1 == 8 && num2 == 9)
 				continue;
