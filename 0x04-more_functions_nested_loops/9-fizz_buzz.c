e4fc47574aeb9e4b1657b7bf794c3ee40d5c28e0
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-20 14:58:17 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the numbers from 1 to 100
+ * 3 multiplies print Fizz instead of the number
+ * 5 multiplies print Buzz instead of the number
+ * 3 & 5 multiplies print  FizzBuzz instead of the number
+ * Return: always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char f[] = "Fizz";
+	char b[] = "Buzz";
+	char fb[] = "FizzBuzz";
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i == 100)
+			printf("%s", b);
+		else if ((i % 3 == 0) && (i % 5 ==0))
+			printf("%s ", fb);
+		else if (i % 5 == 0)
+			printf("%s ", b);
+		else
+			printd("%d ", i);
+	}
+	printf("\n");
+	return (0);
+}
