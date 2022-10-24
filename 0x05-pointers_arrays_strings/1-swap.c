4d96cde711fe6f537703e5d58f1e89524c8bfcb8
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-24 06:59:06 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** swap_int - a function that swaps the value of two integers
+/** 
+ * swap_int - a function that swaps the value of two integers
  * @a: input 1
  * @b: input 2
  * Return: integers
