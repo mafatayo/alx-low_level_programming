135916b738428bcf5baef2573fc0895792231e5a
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-24 09:27:41 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,13 +11,13 @@ void puts_half(char *str)
 
 	len = 0;
 
-	for (i = 0;, str != '\0'; i++)
+	for (i = 0; str != '\0'; i++)
 
 		len++;
 
 	n = (len / 2);
 
-	if ((len % 2) == )
+	if ((len % 2) == 1)
 		n = ((len + 1) / 2);
 
 	for (i = n; str[i] = '\0'; i++)
