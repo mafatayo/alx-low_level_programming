af71fe89ef0e68a6e076b5dadc550c0636126fbf
Michael Akorede <fatayomicheal2017@gmail.com>
2022-10-27 14:42:07 -0700

    Task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,20 +11,23 @@
 
 char _*strncat(char *dest, char *src, int n)
 {
+	int len1;
+	int len2;
+
 	int len1 = 0;
-	int len2 = 0;
-	int i;
 
-	while (src[len1] != '\0')
-		len1++;
-
-	while (dest[len2] != '\0')
-		len2++;
-
-	for (i = 0; i < n; i++)
+	while (dest[len1] != '\0')
 	{
-		dest[len2] = src[i];
+		len1++;
+	}
+	len2 = 0;
+
+	while (len2 < n && src[len2] != '\0')
+	{
+		dest[len1] = src[len2];
+		len1++;
 		len2++;
 	}
+	dest[len1] = 0;
 	return (dest);
 }
