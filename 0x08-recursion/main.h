403d94b2cf248fa834c7d96ee0f08b96f36c3557
Michael Akorede <fatayomicheal2017@gmail.com>
2022-11-02 18:30:05 -0700

    Main

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -10,3 +12,5 @@ int is_palindrome(char *s);
 int helper(int a, int b);
 int primeFinder(int a, int b);
 int palindromeChecker(char *str, int len, int i);
+
+#endif
